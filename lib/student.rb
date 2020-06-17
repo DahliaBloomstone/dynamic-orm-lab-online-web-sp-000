5a570e279ae490f8b8bc0ec57091552bae96eb1a
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-17 22:27:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
-class Student < InteractiveRecord 
+#same as the other lab
+class Student < InteractiveRecord
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
